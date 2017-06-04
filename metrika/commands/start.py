8b1faf863b4b3ba97f1f389d345cb8c81e41cb1b
Taly Guryn <vitalik7tv@yandex.ru>
2017-06-04 23:25:17 +0300

    Generate token for chat

--- metrika/commands/start.py
+++ metrika/commands/start.py
@@ -1,15 +1,40 @@
 from .base import CommandBase
 from config import METRIKA_OAUTH_APP_ID
+import random
 
 class CommandStart(CommandBase):
+
+    @staticmethod
+    def generate_user_token():
+        return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(8))
+
+    """
+    We will store user-to-chat linking in this collection
+    _id | chat | user | dt_register
+    """
+    USERS_COLLECTION_NAME = 'users'
 
     async def __call__(self, payload):
         self.sdk.log("/start handler fired with payload {}".format(payload))
 
+        registered_chat = self.sdk.db.find_one(self.USERS_COLLECTION_NAME, {'chat': payload['chat']})
+
+        if registered_chat:
+            user_token = registered_chat['user']
+        else:
+            user_token = self.generate_user_token()
+            new_chat = {
+                'chat': payload['chat'],
+                'user': user_token,
+                'dt_register': time()
+            }
+            self.sdk.db.insert(self.USERS_COLLECTION_NAME, new_chat)
+            self.sdk.log("New user registered with token {}".format(user_token))
+
         message = "Перейдите по следующей ссылке для авторизации.\n" \
                   "\n" \
                   "https://oauth.yandex.ru/authorize?" \
-                  "response_type=code&client_id={}&state={}".format(METRIKA_OAUTH_APP_ID, payload['chat'])
+                  "response_type=code&client_id={}&state={}".format(METRIKA_OAUTH_APP_ID, user_token)
 
         await self.sdk.send_to_chat(
             payload["chat"],
--- metrika/main.py
+++ metrika/main.py
@@ -3,7 +3,6 @@ from config import APPLICATION_TOKEN, APPLICATION_NAME, DB, SERVER
 from commands.help import CommandHelp
 from commands.start import CommandStart
 
-
 class Metrika:
 
     def __init__(self):
@@ -18,10 +17,30 @@ class Metrika:
         ])
 
         self.sdk.set_routes([
-            #('POST', '/metrika/callback', self.metrika_route_handler)
+            ('GET', '/metrika/callback', self.metrika_route_handler)
         ])
 
         self.sdk.start_server()
+
+    @CodexBot.http_response
+    async def metrika_route_handler(self, request):
+        # self.sdk.log("Callback from yandex.metrika auth with request {}".format(request))
+
+        print(request['query'])
+
+        message = "Авторизация прошла успешно."
+
+        self.sdk.log(message)
+
+        # await self.sdk.send_to_chat(
+        #     # payload["chat"],
+        #     '2JMAGGW9',
+        #     message
+        # )
+
+        return {
+            'text': 'OK!'
+        }
 
 
 if __name__ == "__main__":
